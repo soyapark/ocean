6339f10a039bcd38eb15dd03e8e0ae0131426e97
soyapark <soya@mit.edu>
2021-06-25 15:48:26 -0400

    summary demo

---
+++ web/ziva-summary/app.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $('.summary-text').hide();
+});
+
+function switchSummary(e) {
+    console.log(e);
+
+    if (e.target.checked) {
+        $('.summary-text').show();
+        $('.full-text').hide();
+    } else {
+        $('.summary-text').hide();
+        $('.full-text').show();
+    }
+}

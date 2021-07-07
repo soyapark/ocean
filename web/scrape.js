469ee035d3d37482832bb5ef173669366b5f8fd9
Soya <33616044+soyapark@users.noreply.github.com>
2021-07-07 14:17:39 -0400

    right click menu bar to close extra

--- web/scrape.js
+++ web/scrape.js
@@ -1,3 +1,4 @@
+let $right_clicked_tgt;
 $(document).ready(function () {
     /*$('.navbar').hover(function()
     {
@@ -33,11 +34,20 @@ $(document).ready(function () {
             scrape = $("<p></p>").append(scrape);
 
         scrape.css("border", "1rem solid grey");
-        $($(e.target.parentElement).attr("href")).replaceWith("<p>(skipped)</p>");
 
-        $(e.currentTarget).find(".appended-text:first").append(
-            scrape
-        );
+        // if already appended
+        if ($(e.target.parentElement).find("> .appended-text").text()) {
+            $(e.target.parentElement).find("> .appended-text").show();
+        }
+        else {
+            $($(e.target.parentElement).attr("href")).replaceWith("<p>(skipped)</p>");
+            $(e.target.parentElement).find("> .appended-text").append(
+                scrape
+            ).show();
+        }
+            
+
+        
 
         if ($(e.target.parentElement).attr("href") == "#rule-model") {
             $(window).scrollTop(
@@ -53,4 +63,49 @@ $(document).ready(function () {
         // if( $(`#${s}-${$(e.target).attr("href").substring(1)}`).length == 0 )   
         //   $( `<a role="button" id="${s}-${$(e.target).attr("href").substring(1)}" href="#${s}">${BRIDGE_ICON}</a>` ).insertAfter( $(e.target).attr("href") );
     })
+
+    // disable right click and show custom context menu
+    $("p").bind('contextmenu', function (e) {
+        if ($(e.target).parents(".appended-text").length == 0) {
+            return;
+        }
+        var id = this.id;
+        $("#txt_id").val(id);
+
+        $right_clicked_tgt = $(e.target).parents(".appended-text");
+
+        var top = e.pageY + 5;
+        var left = e.pageX;
+
+        // Show contextmenu
+        $(".context-menu").toggle(100).css({
+            top: top + "px",
+            left: left + "px"
+        });
+
+        // disable default context menu
+        return false;
+    });
+
+    // Hide context menu
+    $(document).bind('contextmenu click', function () {
+        $(".context-menu").hide();
+        $("#txt_id").val("");
+    });
+
+    // disable context-menu from custom menu
+    $('.context-menu').bind('contextmenu', function () {
+        return false;
+    });
+
+    // Clicked context-menu item
+    $('.context-menu li').click(function () {
+        debugger;
+        var className = $(this).find("span:nth-child(1)").attr("class");
+        var titleid = $('#txt_id').val();
+        $("#" + titleid).css("background-color", className);
+        $right_clicked_tgt.hide();
+        $right_clicked_tgt.parents(".ourLink").find('.btn').show();
+        $(".context-menu").hide();
+    });
 }); 

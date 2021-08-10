5e7fedee8e449b36229ce7b403bc363fac3d144b
Soya <33616044+soyapark@users.noreply.github.com>
2021-08-10 13:42:48 -0400

    back button

--- annotation/recogito-firebase-storage.js
+++ annotation/recogito-firebase-storage.js
@@ -2,5 +2,11 @@
     return r.collection(o).where("id","==",n).get().then(function(n){return n.docs[0]})}var r=firebase.firestore();
     r.collection(o) //.where("target.source","==",e)
     .get().then(function(n){
-        n=n.docs.map(function(n){return n.data()});t.setAnnotations(n)
+        n=n.docs.map(function(n){return n.data()});
+        // t.setAnnotations(n);
+
+        for(var i=0;i< n.length; i++)
+            t.addAnnotation(n[i]);
+
+        document.querySelectorAll(".r6o-annotation").forEach(e => e.id = e.getAttribute("data-id").substr(1));
 }),t.on("createAnnotation",function(t){r.collection(o).add(t).catch(function(n){return console.error("Error storing annotation",n,t)})}),t.on("updateAnnotation",function(t,e){i(e.id).then(function(n){return n.ref.update(t)}).catch(function(n){return console.log("Error updating annotation",n,e,t)})}),t.on("deleteAnnotation",function(t){i(t.id).then(function(n){return n.ref.delete()}).catch(function(n){return console.log("Error deleting annotation",n,t)})})}});
